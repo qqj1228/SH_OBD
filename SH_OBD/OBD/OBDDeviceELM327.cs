43bcce4fc30dc9d53a35e0bba30afa572aa0a133
qqj1228 <qqj1228@gmail.com>
2020-07-30 11:40:15 +0800

    change 11bit CAN filter and mask

--- SH_OBD/OBD/OBDDeviceELM327.cs
+++ SH_OBD/OBD/OBDDeviceELM327.cs
@@ -146,8 +146,8 @@ namespace SH_OBD {
                 break;
             case ProtocolType.ISO_15765_4_CAN_11BIT_500KBAUD:
             case ProtocolType.ISO_15765_4_CAN_11BIT_250KBAUD:
-                ConfirmAT("ATCF7E0");
-                ConfirmAT("ATCM7F0");
+                ConfirmAT("ATCF700");
+                ConfirmAT("ATCMF00");
                 for (int i = 3; i > 0 && !bflag; i--) {
                     if (GetOBDResponse("0100").Replace(" ", "").Contains("4100")) {
                         bflag = true;
